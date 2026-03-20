b5547473ef106002bac4e6341e6fb45bb652342f
vinrohman <m.rohman@teravintech.com>
2026-03-20 21:37:32 +0700

    ui: Replace Lexend with OpenSans font family and update Typography configuration

--- shared/common-ui/src/main/java/dev/muffar/moneyfikasi/common_ui/theme/Type.kt
+++ shared/common-ui/src/main/java/dev/muffar/moneyfikasi/common_ui/theme/Type.kt
@@ -8,66 +8,65 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import dev.muffar.moneyfikasi.resource.R
 
-val LexendFontFamily = FontFamily(
-    Font(R.font.lexend_light, FontWeight.Normal),
-    Font(R.font.lexend_regular, FontWeight.Medium),
-    Font(R.font.lexend_medium, FontWeight.SemiBold),
-    Font(R.font.lexend_semibold, FontWeight.Bold),
-    Font(R.font.lexend_bold, FontWeight.ExtraBold),
+val OpenSansFontFamily = FontFamily(
+    Font(R.font.open_sans_regular, FontWeight.Normal),
+    Font(R.font.open_sans_medium, FontWeight.Medium),
+    Font(R.font.open_sans_semibold, FontWeight.SemiBold),
+    Font(R.font.open_sans_bold, FontWeight.Bold),
 )
 
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         lineHeight = 64.sp,
         fontSize = 57.sp,
         letterSpacing = 0.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         lineHeight = 52.sp,
         fontSize = 45.sp,
         letterSpacing = 0.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         lineHeight = 44.sp,
         fontSize = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Bold,
         lineHeight = 40.sp,
         fontSize = 32.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Bold,
         lineHeight = 36.sp,
         fontSize = 28.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Bold,
         lineHeight = 32.sp,
         fontSize = 24.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 28.sp,
         fontSize = 22.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 24.sp,
         fontSize = 16.sp,
@@ -75,49 +74,49 @@ val Typography = Typography(
 
     ),
     titleSmall = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.SemiBold,
         lineHeight = 20.sp,
         fontSize = 14.sp,
         letterSpacing = 0.1.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Medium,
         lineHeight = 24.sp,
         fontSize = 16.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Medium,
         lineHeight = 16.sp,
         fontSize = 12.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Medium,
         lineHeight = 16.sp,
         fontSize = 11.sp,
         letterSpacing = 0.5.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         lineHeight = 24.sp,
         fontSize = 16.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         lineHeight = 20.sp,
         fontSize = 14.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = LexendFontFamily,
+        fontFamily = OpenSansFontFamily,
         fontWeight = FontWeight.Normal,
         lineHeight = 16.sp,
         fontSize = 12.sp,
