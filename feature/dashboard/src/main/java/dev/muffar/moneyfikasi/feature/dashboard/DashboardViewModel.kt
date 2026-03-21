ccc02571a98693b235fa4aee31de47c3a2cae099
vinrohman <m.rohman@teravintech.com>
2026-03-21 20:55:08 +0700

    refactor: Integrate PreferencesUseCases and update balance visibility logic in DashboardViewModel

--- feature/dashboard/src/main/java/dev/muffar/moneyfikasi/feature/dashboard/DashboardViewModel.kt
+++ feature/dashboard/src/main/java/dev/muffar/moneyfikasi/feature/dashboard/DashboardViewModel.kt
@@ -3,17 +3,20 @@ package dev.muffar.moneyfikasi.feature.dashboard
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.muffar.moneyfikasi.domain.usecase.preferences.PreferencesUseCases
 import dev.muffar.moneyfikasi.domain.usecase.wallet.WalletUseCases
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
     private val walletUseCases: WalletUseCases,
+    private val preferencesUseCases: PreferencesUseCases,
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(DashboardState())
@@ -21,6 +24,7 @@ class DashboardViewModel @Inject constructor(
 
     init {
         getWallets()
+        getPreferences()
     }
 
     private fun getWallets() {
@@ -33,12 +37,23 @@ class DashboardViewModel @Inject constructor(
             }.launchIn(viewModelScope)
     }
 
+    private fun getPreferences() {
+        preferencesUseCases.isBalanceVisible()
+            .onEach { isVisible ->
+                _state.update { it.copy(isBalanceVisible = isVisible) }
+            }.launchIn(viewModelScope)
+    }
+
     fun onEvent(event: DashboardEvent) {
         when (event) {
             is DashboardEvent.Refresh -> getWallets()
-            is DashboardEvent.ToggleBalanceVisibility -> {
-                _state.update { it.copy(isBalanceVisible = !it.isBalanceVisible) }
-            }
+            is DashboardEvent.ToggleBalanceVisibility -> toggleBalanceVisibility()
+        }
+    }
+
+    private fun toggleBalanceVisibility() {
+        viewModelScope.launch {
+            preferencesUseCases.setBalanceVisibility(!_state.value.isBalanceVisible)
         }
     }
 }
