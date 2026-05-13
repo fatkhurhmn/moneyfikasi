940293f8755e9174d49814a86b00ebed77abd8b6
vinrohman <m.rohman@teravintech.com>
2026-05-13 16:28:13 +0700

    feat: refactor color palette, implement semantic naming, and update theme configurations

--- shared/common-ui/src/main/java/dev/muffar/moneyfikasi/common_ui/theme/color/MainColor.kt
+++ shared/common-ui/src/main/java/dev/muffar/moneyfikasi/common_ui/theme/color/MainColor.kt
@@ -6,50 +6,45 @@ import androidx.compose.ui.graphics.Color
 @Immutable
 object MainColor {
 
-    val White          = Color(0xFFFFFFFF)
-    val ExtraLightGray = Color(0xFFF7F8FA)
-    val LightGray      = Color(0xFFE4E6EA)
-    val Gray           = Color(0xFF95A0AD)
-    val DarkGray       = Color(0xFF4A5568)
-    val ExtraDarkGray  = Color(0xFF121417)
-    val Black          = Color(0xFF000000)
+    // Brand / Primary
+    val Primary = Color(0xFFC6F00D)
+    val PrimaryHover = Color(0xFFB2DB0C)
+    val PrimaryContainerLight = Color(0xFFEAF8B1)
+    val PrimaryContainerDark = Color(0xFF2A3408)
+    val OnPrimary = Color(0xFF121417)
 
-    val SurfaceDarkPage = Color(0xFF121417)
-    val SurfaceDarkCard = Color(0xFF1C1F22)
-    val SurfaceDarkRow  = Color(0xFF25292D)
+    // Light Theme Surfaces
+    val LightBackground = Color(0xFFF7F8FA)
+    val LightSurface1 = Color(0xFFFFFFFF)
+    val LightSurface2 = Color(0xFFEFF2F5)
+    val LightSurface3 = Color(0xFFE4E9EE)
+    val LightOutline = Color(0xFFD7DDE4)
+    val LightTextPrimary = Color(0xFF121417)
+    val LightTextSecondary = Color(0xFF4A5568)
 
-    val LimeExtraLight = Color(0xFFEAF8B1)
-    val LimeKindaLight = Color(0xFFD9F26D)
-    val Lime           = Color(0xFFC6F00D)
-    val LimeDark       = Color(0xFF9EBF0A)
-    val LimeExtraDark  = Color(0xFF7A9408)
+    // Dark Theme Surfaces
+    val DarkBackground = Color(0xFF121417)
+    val DarkSurface1 = Color(0xFF1C1F22)
+    val DarkSurface2 = Color(0xFF25292D)
+    val DarkSurface3 = Color(0xFF2F3439)
+    val DarkOutline = Color(0xFF3B424A)
+    val DarkTextPrimary = Color(0xFFF7F8FA)
+    val DarkTextSecondary = Color(0xFFC9D1DA)
 
-    val Charcoal       = Color(0xFF121417)
-    val CharcoalLight  = Color(0xFF2C3E50)
+    // Semantic Colors
+    val Success = Color(0xFF2DC46D)
+    val SuccessContainer = Color(0xFFE8F8EF)
+    val Error = Color(0xFFEE5040)
+    val ErrorContainer = Color(0xFFFDECEB)
+    val Warning = Color(0xFFF39C12)
+    val WarningContainer = Color(0xFFFEF4E6)
+    val Info = Color(0xFF2563EB)
 
-    val GreenExtraLight = Color(0xFFE8F8EF)
-    val GreenKindaLight = Color(0xFF6DCE99)
-    val Green           = Color(0xFF2DC46D)
-    val GreenDark       = Color(0xFF1C8E4D)
-    val GreenExtraDark  = Color(0xFF126C38)
+    // Basic Colors
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
 
-    val RedExtraLight = Color(0xFFFDECEB)
-    val RedKindaLight = Color(0xFFEF9088)
-    val Red           = Color(0xFFEE5040)
-    val RedDark       = Color(0xFFB33829)
-    val RedExtraDark  = Color(0xFF972B1E)
+    val ExtraLightGray = LightBackground
 
-    val OrangeExtraLight = Color(0xFFFEF4E6)
-    val OrangeKindaLight = Color(0xFFF7C068)
-    val Orange           = Color(0xFFF39C12)
-    val OrangeDark       = Color(0xFFB07208)
-    val OrangeExtraDark  = Color(0xFF8A5806)
-
-    val BlueExtraLight = Color(0xFFEFF6FF)
-    val BlueKindaLight = Color(0xFF60A5FA)
-    val Blue           = Color(0xFF2563EB)
-    val BlueKindaDark  = Color(0xFF1D4ED8)
-
-    val YellowKindaDark  = Color(0xFFFFC300)
-    val YellowDark       = Color(0xFFF9A825)
+    val YellowDark = Color(0xFFF9A825)
 }
