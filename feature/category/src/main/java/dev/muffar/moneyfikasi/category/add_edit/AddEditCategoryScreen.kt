dc8e2c5305bcd46c2114ee5549a27deb00c85a38
vinrohman <m.rohman@teravintech.com>
2026-05-14 21:21:49 +0700

    feat: update AddEditCategoryScreen and top bar to use surface color

--- feature/category/src/main/java/dev/muffar/moneyfikasi/category/add_edit/AddEditCategoryScreen.kt
+++ feature/category/src/main/java/dev/muffar/moneyfikasi/category/add_edit/AddEditCategoryScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
@@ -47,9 +48,11 @@ fun AddEditCategoryScreen(
     val scrollState = rememberScrollState()
 
     Scaffold(
+        containerColor = MaterialTheme.colorScheme.surface,
         topBar = {
             CommonTopAppBar(
                 title = title,
+                containerColor = MaterialTheme.colorScheme.surface,
                 onBackClick = onBackClick
             )
         },
