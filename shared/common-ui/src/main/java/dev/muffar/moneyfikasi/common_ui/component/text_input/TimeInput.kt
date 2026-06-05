4389d4c46c2534f5363b97c039849ed328cae526
vinrohman <m.rohman@teravintech.com>
2026-06-05 21:06:36 +0700

    feat: update TimeInput to use LocalLocale for time formatting, replacing the global Locale.getDefault with the Compose-aware platform locale

--- shared/common-ui/src/main/java/dev/muffar/moneyfikasi/common_ui/component/text_input/TimeInput.kt
+++ shared/common-ui/src/main/java/dev/muffar/moneyfikasi/common_ui/component/text_input/TimeInput.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLocale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import dev.muffar.moneyfikasi.common_ui.component.bottom_sheet.TimePickerSheet
 import dev.muffar.moneyfikasi.resource.R
-import java.util.Locale
 
 @Composable
 fun TimeInput(
@@ -29,8 +29,9 @@ fun TimeInput(
     Column(
         modifier = modifier
     ) {
+        val locale = LocalLocale.current.platformLocale
         CommonTextInput(
-            value = String.format(Locale.getDefault(), "%02d:%02d", time.first, time.second),
+            value = String.format(locale, "%02d:%02d", time.first, time.second),
             onValueChange = {},
             label = stringResource(R.string.label_time),
             isClickable = true,
